d6b0107f438087793db078799b905d8d6d9c5e8a
kbseah <kbseah@users.noreply.github.com>
2018-06-15 19:27:24 +0200

    Modify diffGetPeaks to select massdiffs by parent mass

--- R/diffGetPeaks.R
+++ R/diffGetPeaks.R
@@ -1,18 +1,25 @@
-#' Find pairs of mass peaks corresponding to a specific mass difference
+#' Find pairs of mass peaks corresponding to a specific mass difference (putative adduct)
 #'
 #' @param d data.frame; output from diffTabulate
-#' @param mass numeric; mass difference in m/z units
+#' @param by string; Subset the mass by mass difference (putative adduct
+#'                   transformation) ("diff") or by parent ion ("parent")?
+#' @param mass numeric; mass of putative adduct or parent ion in m/z units
 #' @param width numeric; range of values to use
 #'
-#' @return data.frame of peak pairs where the difference is within the
-#' width of the specified mass of interest
+#' @return subset of the original data frame d where the mass difference or
+#'         parent ion is close to the specified mass of interest (within the
+#'         specified width)
 #' @export
 
-diffGetPeaks <- function(d, mass=NULL, width=0.01) {
+diffGetPeaks <- function(d, by="diff", mass=NULL, width=0.01) {
     if (! is.null (mass)) {
         diffLow <- mass - width/2
         diffUpp <- mass + width/2
-        output <- d[which(d$diff > diffLow & d$diff <= diffUpp),]
+        if (by=="diff") {
+            output <- d[which(d$diff > diffLow & d$diff <= diffUpp),]
+        } else if (by == "parent") {
+            output <- d[which(d$A > diffLow & d$A <= diffUpp),]
+        } 
         return (output)
     } else {
         cat ("Error: Mass difference not specified\n")
