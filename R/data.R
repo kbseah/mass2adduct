dba112ae8f620788ee11cc3952f16f1808c62307
kbseah <kbseah@users.noreply.github.com>
2018-06-15 16:42:54 +0200

    Edit included adduct data

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing the masses and formulae of selected adducts commonly
 #' observed in mass spectrometry imaging data.
 #'
-#' @format A data.frame with 92 rows and 3 columns:
+#' @format A data.frame with 97 rows and 3 columns:
 #' \describe{
 #'   \item{name}{Common name of adduct}
 #'   \item{formula}{Formula of adduct}
@@ -11,3 +11,17 @@
 #' }
 #' @source Moritz Janda, Manuel Liebeke
 "adducts"
+
+#' Shorter list of mass spectrometry adducts
+#'
+#' A dataset containing the masses and formulae of selected adducts commonly
+#' observed in mass spectrometry imaging data, subset of the list \code{adducts}
+#'
+#' @format A data.frame with 9 rows and 3 columns:
+#' \describe{
+#'   \item{name}{Common name of adduct}
+#'   \item{formula}{Formula of adduct}
+#'   \item{mass}{Mass of adduct in m/z units}
+#' }
+#' @source Moritz Janda, Manuel Liebeke
+"adducts2"
