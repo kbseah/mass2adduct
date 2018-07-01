c61c70ac726894f6fed4feb106da4129f792454c
kbseah <kbseah@mpi-bremen.de>
2018-07-01 23:44:44 +0200

    Change "parental" to "parent" for consistency

--- R/pointsAdducts.R
+++ R/pointsAdducts.R
@@ -1,13 +1,17 @@
 #' Overlay adduct parental and adduct masses on plot of msimat object
 #'
 #' On a mass spectrum plot (from an msimat object), plot points representing
-#' either parental or adduct masses as an overlay. 
+#' either parental or adduct masses as an overlay. By default only those flagged
+#' as having statistically significant spatial correlation will be plotted. The
+#' massdiff object should have been processed with \code{\link{adductMatch}} and
+#' \code{\link{corrPairsMSI}} to annotate with adduct matches and correlation
+#' test results. 
 #'
 #' @param d msimat; Object of class msimat created by function
 #'        \code{\link{msimat}}
 #' @param diff data.frame; Mass difference correlation table produced by
 #'        function \code{\link{corrPairsMSI}}
-#' @param which string; Either plot "parental" or "adduct" peaks as overlay
+#' @param which string; Either plot "parent" or "adduct" peaks as overlay
 #' @param signif logical; Only plot points which have statistically significant
 #'        correlation
 #' @param pch Plot character to use, passed to \code{points}
@@ -19,13 +23,13 @@
 #' @seealso \code{\link{massdiff}} tabulate all possible mass pairs
 #' @export
 
-pointsAdducts <- function(d, diff, which=c("adduct","parental"), signif=TRUE, pch=20, cex=0.5, col="red", ...) {
+pointsAdducts <- function(d, diff, which=c("adduct","parent"), signif=TRUE, pch=20, cex=0.5, col="red", ...) {
   if (signif) {
     diff <- diff[which(diff$Significance == 1),]
   }
   if (which[1] == "adduct") {
     shortlist <- diff$B
-  } else if (which[1] == "parental") {
+  } else if (which[1] == "parent") {
     shortlist <- diff$A
   }
   # It is possible that there are duplicates in shortlist because there may be
