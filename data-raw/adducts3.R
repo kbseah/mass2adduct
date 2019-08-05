98b48cac8bd9b52f7bf8e543b203db0f4c325562
Dennis Jakob <dennisjakob@gmx.de>
2019-08-05 14:59:42 +0200

    typo corrected

--- data-raw/adducts3.R
+++ data-raw/adducts3.R
@@ -1,5 +1,5 @@
 library(usethis)
 
-adducts3 <- read.table("adducts3.tsv",sep="\t",header=T)
+adducts3 <- read.table("data-raw/adducts3.tsv",sep="\t",header=T)
 
 usethis::use_data(adducts3,overwrite=TRUE)
